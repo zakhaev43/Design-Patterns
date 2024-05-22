91a2c659fb31efd0a2eb46196a772f92180b0928
imransuet <imransuet@gmail.com>
2024-05-22 10:49:11 +0200

    Helloworldnew

--- main.go
+++ main.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
-func starter(wg *sync.WaitGroup) {
-	fmt.Println("This is the starter on call")
-	defer wg.Done()
-}
-
-func follow() {
-	fmt.Println("This is the follower on call")
-}
-
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go starter(&wg)
-	follow()
-	wg.Wait()
+	fmt.Println("Hello World")
 }
