7049fbe4058ce07583da63dd0f0ea7785b4ac074
zakhaev43 <imransuet@gmail.com>
2024-07-15 22:36:56 +0200

    nesting tested

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "github.com/zakhaev43/Golang-Practice/design_pattern"
+import "fmt"
+
+type Stringer interface {
+	String() string
+}
+
+type Book struct {
+	Title  string
+	Author string
+}
+
+func (b *Book) String() string {
+	return fmt.Sprintf("Book: %s - %s", b.Title, b.Author)
+}
+
+type Library struct {
+	Name  string
+	Books []Book
+}
+
+func (l *Library) String() string {
+	result := fmt.Sprintf("Library: %s\n", l.Name)
+	for _, book := range l.Books {
+		result += book.String() + "\n"
+	}
+	return result
+}
 
 func main() {
-    design_pattern.Go_Decorator()
+	library := &Library{
+		Name: "City Library",
+		Books: []Book{
+			{Title: "كتاب الحجة على أهل المدينة", Author: "محمد بن حسن الشيباني"},
+			{Title: "المبسوط", Author: "محمد بن حسن الشيباني"},
+			{Title: "كتاب السير الكبير", Author: "محمد بن حسن الشيباني"},
+		},
+	}
+
+	fmt.Println(library.Books)
 }
